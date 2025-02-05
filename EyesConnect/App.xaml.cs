39a51f1d312a31b5502eceaa7b644e287bf6def2
mattiagm08 <mattiagimmillaro48@gmail.com>
2025-02-05 17:08:33 +0100

    EyesConnect Version 0.0.1

---
+++ EyesConnect/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EyesConnect
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ EyesConnect/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EyesConnect
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
